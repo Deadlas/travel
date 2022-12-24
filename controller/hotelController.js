62a6d1937488bf591ff21663eeb7729ba99021d7
Deadlas1998 <Deadlas1998@outlook.com>
2022-12-24 08:43:34 -0700

    photo problem fixed

--- controller/hotelController.js
+++ controller/hotelController.js
@@ -7,7 +7,7 @@ const searchLocation = (req, res) => {
 		url: 'https://apidojo-booking-v1.p.rapidapi.com/locations/auto-complete',
 		params: { text: req.query.query, languagecode: 'en-us' },
 		headers: {
-			'X-RapidAPI-Key': 'eb190b2daamshb66f78e68920f4cp1e74c9jsn76bb6cb8725f',
+			'X-RapidAPI-Key': '5c476d6342msh05ee9313efa0affp17029cjsnae85b77e1954',
 			'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
 		}
 	};
@@ -23,25 +23,25 @@ const getHotelsFilter = (req, res) => {
 		method: 'GET',
 		url: 'https://apidojo-booking-v1.p.rapidapi.com/properties/list',
 		params: {
-		  offset: '0',
-		  arrival_date: req.query.arrival_date,
-		  departure_date: req.query.departure_date,
-		  guest_qty: req.query.guest_qty,
-		  dest_ids: req.query.dest_ids,
-		  room_qty: req.query.room_qty,
-		  search_type: req.query.search_type,
-		  children_qty: req.query.children_qty,
-		  search_id: req.query.search_id,
-		  price_filter_currencycode: 'USD',
-		  order_by: 'popularity',
-		  languagecode: 'en-us',
-		  travel_purpose: 'leisure'
+			offset: '0',
+			arrival_date: req.query.arrival_date,
+			departure_date: req.query.departure_date,
+			guest_qty: req.query.guest_qty,
+			dest_ids: req.query.dest_ids,
+			room_qty: req.query.room_qty,
+			search_type: req.query.search_type,
+			children_qty: req.query.children_qty,
+			search_id: req.query.search_id,
+			price_filter_currencycode: 'USD',
+			order_by: 'popularity',
+			languagecode: 'en-us',
+			travel_purpose: 'leisure'
 		},
 		headers: {
-		  'X-RapidAPI-Key': 'eb190b2daamshb66f78e68920f4cp1e74c9jsn76bb6cb8725f',
-		  'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
+			'X-RapidAPI-Key': '5c476d6342msh05ee9313efa0affp17029cjsnae85b77e1954',
+			'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
 		}
-	  };
+	};
 	axios.request(options).then(function (response) {
 		res.json(response.data);
 	}).catch(function (error) {
@@ -50,39 +50,38 @@ const getHotelsFilter = (req, res) => {
 }
 
 const searchHotels = (req, res) => {
-	if(req.query.categories_filter==undefined)
-	{
-		req.query.categories_filter=[];
+	if (req.query.categories_filter == undefined) {
+		req.query.categories_filter = [];
 	}
 	const options = {
 		method: 'GET',
 		url: 'https://apidojo-booking-v1.p.rapidapi.com/properties/list',
 		params: {
-		  offset: '0',
-		  arrival_date: req.query.arrival_date,
-		  departure_date: req.query.departure_date,
-		  guest_qty: req.query.guest_qty,
-		  dest_ids: req.query.dest_ids,
-		  room_qty: req.query.room_qty,
-		  search_type: req.query.search_type,
-		  children_qty: req.query.children_qty,
-		  search_id: req.query.search_id,
-		  price_filter_currencycode: 'USD',
-		  order_by: 'popularity',
-		  languagecode: 'en-us',
-		  travel_purpose: 'leisure',
-		  search_id:req.query.search_id,
-		  categories_filter:req.query.categories_filter.toString()
+			offset: '0',
+			arrival_date: req.query.arrival_date,
+			departure_date: req.query.departure_date,
+			guest_qty: req.query.guest_qty,
+			dest_ids: req.query.dest_ids,
+			room_qty: req.query.room_qty,
+			search_type: req.query.search_type,
+			children_qty: req.query.children_qty,
+			search_id: req.query.search_id,
+			price_filter_currencycode: 'USD',
+			order_by: 'popularity',
+			languagecode: 'en-us',
+			travel_purpose: 'leisure',
+			search_id: req.query.search_id,
+			categories_filter: req.query.categories_filter.toString()
 		},
 		headers: {
-		  'X-RapidAPI-Key': 'eb190b2daamshb66f78e68920f4cp1e74c9jsn76bb6cb8725f',
-		  'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
+			'X-RapidAPI-Key': '5c476d6342msh05ee9313efa0affp17029cjsnae85b77e1954',
+			'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
 		}
-	  };
+	};
 	const baseurl = "http://localhost:3030/"
 	axios.request(options).then(function (response) {
-		// res.json({ "scrapped": response.data, "base": baseurl, "params": req.query });
-		res.render("../views/searchResult", { "scrapped": response.data, "base": baseurl, "params": req.query, });
+		res.json({ "scrapped": response.data, "base": baseurl, "params": req.query });
+		// res.render("../views/searchResult", { "scrapped": response.data, "base": baseurl, "params": req.query, });
 	}).catch(function (error) {
 		// console.error(error);
 	});
@@ -103,7 +102,7 @@ const getHotelDetails = (req, res) => {
 			'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
 		}
 	};
-	
+
 
 	axios.request(options).then(function (response) {
 		res.json(response.data);
